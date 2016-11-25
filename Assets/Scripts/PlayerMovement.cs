2e9044a99aa92eec161f93fc6eabfacd6933bced
Samvid95 <samvid95@gmail.com>
2016-11-25 14:22:39 +0530

    The movement is not working properly!!

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float movementSpeed;
+    
+    private float moveVelocity;
+   
+
+   
+
+	// Use this for initialization
+	void Start () {
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        moveVelocity = 0;
+       
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.R))
+        {
+            Debug.Log("Right Arrow is pressed!!");
+            moveVelocity += movementSpeed;                
+        }
+
+         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.L))
+         {
+            Debug.Log("Left Arrow is pressed!!");
+            moveVelocity -= movementSpeed;                
+         }
+
+        Debug.Log("The movement velocity should be:" + new Vector2(moveVelocity,GetComponent<Rigidbody2D>().velocity.y)); 
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+        
+    }
+
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        
+
+    }
+    
+    void OnCollisionExit2D(Collision2D col)
+    {
+       
+    }
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+    public float moveSpeed;
+
+    float moveVelocity;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveVelocity = 0.0f;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity -= moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity += moveSpeed;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+    }
+}
